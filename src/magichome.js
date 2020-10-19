e7d5961a2987b54ad1219a464d1a10afb6331e0b
SynTexDZN <syntexdzn@t-online.de>
2020-10-19 19:53:40 +0200

    Changing Device Management and Webserver Handeling

--- src/magichome.js
+++ src/magichome.js
@@ -282,6 +282,19 @@ function validateUpdate(mac, letters, state)
     }
 }
 
+var types = ['contact', 'motion', 'temperature', 'humidity', 'rain', 'light', 'occupancy', 'smoke', 'airquality', 'rgb', 'switch', 'relais', 'statelessswitch'];
+var letters = ['A', 'B', 'C', 'D', 'E', 'F', '0', '1', '2', '3', '4', '5', '6'];
+
+function letterToType(letter)
+{
+    return types[letters.indexOf(letter.toUpperCase())];
+}
+
+function typeToLetter(type)
+{
+    return letters[types.indexOf(type.toLowerCase())];
+}
+
 function MagicHomeGlobals() {}
 
 MagicHomeGlobals.setHomebridge = function(homebridgeRef)
