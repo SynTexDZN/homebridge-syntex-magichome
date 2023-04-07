6e25c9f56f24a8eaba45f2d5e83bfd256ff5fb43
SynTex <syntexdzn@t-online.de>
2023-04-07 18:56:37 +0200

    Performance Improvements Changed Refresh Accessory System

--- src/device-manager.js
+++ src/device-manager.js
@@ -5,6 +5,8 @@ module.exports = class DeviceManager
 	constructor(platform)
 	{
 		this.logger = platform.logger;
+
+		this.TypeManager = platform.TypeManager;
 	}
 
 	getDevice(service, callback)
@@ -43,56 +45,119 @@ module.exports = class DeviceManager
 		});
 	}
 
+	getDevices(ips, callback)
+	{
+		this.executeCommand(ips, '-i', (error, output) => {
+			
+			var data = output.split('\n'), states = {};
+
+			data.pop();
+
+			this.logger.debug(data);
+
+			for(const i in data)
+			{
+				var ip = data[i].match(/[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*|(([0-9]|[a-f])*:([0-9]|[a-f])*:([0-9]|[a-f])*:([0-9]|[a-f])*:([0-9]|[a-f])*:([0-9]|[a-f])*:([0-9]|[a-f])*:([0-9]|[a-f])*)/g);
+
+				if(Array.isArray(ip) && ip.length > 0)
+				{
+					var offline = data[i].includes('Unable to connect to bulb'), state = {};
+
+					ip = ip[0];
+
+					if(!offline)
+					{
+						var power = data[i].match(/\] ON /g),
+							colors = data[i].match(/\(.*,.*,.*\)/g);
+
+						state.value = (Array.isArray(power) && power.length > 0);
+
+						if(Array.isArray(colors) && colors.length > 0)
+						{
+							var converted = colors[0].slice(1).slice(0, -1).split(',').map((item) => item.trim());
+
+							state.red = converted[0];
+							state.green = converted[1];
+							state.blue = converted[2];
+						}
+					}
+
+					state.connection = !offline;
+
+					states[ip] = state;
+				}
+			}
+
+			callback(states);
+
+		}, false);
+	}
+
 	refreshAccessories(accessories)
 	{
+		var ips = [];
+
 		this.logger.debug('%device_refresh% ..');
 
 		for(const accessory of accessories)
 		{
-			if(Array.isArray(accessory[1].services))
+			for(const service of accessory[1].service)
 			{
-				for(const i in accessory[1].services)
+				if(this.TypeManager.letterToType(service.letters) == 'rgb' && service.ip != null && !ips.includes(service.ip))
 				{
-					if(accessory[1].services[i].type.startsWith('rgb'))
-					{
-						this.getDevice(accessory[1].service[parseInt(i) + 1], (state) => {
-							
-							if(accessory[1].service[parseInt(i) + 1].setConnectionState != null
-							&& accessory[1].service[parseInt(i) + 1].updateState != null)
-							{
-								accessory[1].service[parseInt(i) + 1].setConnectionState(state.connection,
-									() => accessory[1].service[parseInt(i) + 1].updateState(state), true);
-							}
-						});
-					}
-				}
-			}
-			else if(accessory[1].services instanceof Object)
-			{
-				if(accessory[1].services.type.startsWith('rgb'))
-				{
-					this.getDevice(accessory[1].service[1], (state) => {
-						
-						if(accessory[1].service[1].setConnectionState != null
-						&& accessory[1].service[1].updateState != null)
-						{
-							accessory[1].service[1].setConnectionState(state.connection,
-								() => accessory[1].service[1].updateState(state), true);
-						}
-					});
+					ips.push(service.ip);
 				}
 			}
 		}
+
+		this.getDevices(ips.join(' '), (states) => {
+
+			for(const ip in states)
+			{
+				for(const accessory of accessories)
+				{
+					for(const service of accessory[1].service)
+					{
+						if(this.TypeManager.letterToType(service.letters) == 'rgb' && service.ip == ip)
+						{
+							if(service.setConnectionState != null && service.updateState != null)
+							{
+								var converted = service.setChannels([states[ip].red, states[ip].green, states[ip].blue]);
+
+								converted = convert.rgb.hsv(converted);
+
+								if(converted != null)
+								{
+									delete states[ip].red;
+									delete states[ip].green;
+									delete states[ip].blue;
+
+									states[ip].hue = converted[0];
+									states[ip].saturation = converted[1];
+									states[ip].brightness = converted[2];
+								}
+
+								service.setConnectionState(states[ip].connection,
+									() => service.updateState(states[ip]), true);
+							}
+						}
+					}
+				}
+			}
+		});
 	}
 
-	executeCommand(address, command, callback)
+	executeCommand(address, command, callback, verbose = true)
 	{
 		const exec = cp.exec;
 		const cmd = path.join(__dirname, './flux_led.py ' + address + ' ' + command);
 
 		exec(cmd, (err, stdOut) => {
 			
-			this.logger.debug(stdOut);
+			if(verbose)
+			{
+				this.logger.debug(stdOut);
+			}
 			
 			if(err)
 			{
